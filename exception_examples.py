59db90202ff323583e9e3b9d834f27fee09d45f5
maxim.hunko <maxim.hunko@developex.net>
2023-02-09 22:11:58 +0200

    Added full exceptions example.

--- exception_examples.py
+++ exception_examples.py
@@ -3,11 +3,30 @@ def fetch(obj, idx):
     return obj[idx]
 
 
+class MyException(Exception):
+    pass
+
+
 try:
     x = 'this is string'
     index = int(input('input index: '))
     fetch(x, index)
     if index == 3:
-        raise IndexError
+        raise ValueError
+    elif index == 4:
+        raise MyException
+    ss = input('input some string')
+    assert len(ss) < 10, 'to long object..'
+
 except IndexError:
     print('bad index')
+except ValueError:
+    print('bad value')
+except MyException:
+    print('my exception')
+except AssertionError as msg:
+    print(f'assert {msg}')
+else:
+    print('else exception')
+finally:
+    print('finally code')
