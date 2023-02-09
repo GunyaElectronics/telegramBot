c462e401827dddeb230de31df787e4bb3aaf9f01
maxim.hunko <maxim.hunko@developex.net>
2023-02-09 22:34:41 +0200

    Added activity class.

---
+++ activity.py
@@ -0,0 +1,8 @@
+class Activity:
+    """class for any activity"""
+
+    def __init__(self, name, unit, count, datetime):
+        self.name = name
+        self.unit = unit
+        self.count = count
+        self.datetime = datetime
